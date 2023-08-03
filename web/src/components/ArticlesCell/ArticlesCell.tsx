848e79aa5c506ce0aa6e5b0bb7c09eff51bef7ad
Alexandre Nakamura <anakamura@bluelab.com.br>
2023-08-03 13:44:59 -0300

    blog com paginas individuais

---
+++ web/src/components/ArticlesCell/ArticlesCell.tsx
@@ -0,0 +1,29 @@
+import type { ArticlesQuery } from 'types/graphql'
+import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
+
+export const QUERY = gql`
+  query ArticlesQuery {
+    articles : posts {
+      id
+      title
+      body
+      createdAt
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => <div>Empty</div>
+
+export const Failure = ({ error }: CellFailureProps) => (
+  <div style={{ color: 'red' }}>Error: {error?.message}</div>
+)
+export const Success = ({ articles }) => {
+  return articles.map((article) => (
+    <article key={article.id}>
+      <h2>{article.title}</h2>
+      <div>{article.body}</div>
+    </article>
+  ))
+}
