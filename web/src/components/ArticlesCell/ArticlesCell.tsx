df24e8f9f0ad7adb76f941e11e26dad98a33796a
Alexandre Nakamura <anakamura@bluelab.com.br>
2023-08-03 14:01:19 -0300

    na pagina principal, links para paginas individuais

--- web/src/components/ArticlesCell/ArticlesCell.tsx
+++ web/src/components/ArticlesCell/ArticlesCell.tsx
@@ -1,5 +1,6 @@
 import type { ArticlesQuery } from 'types/graphql'
 import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
+import { Link, routes } from '@redwoodjs/router'
 
 export const QUERY = gql`
   query ArticlesQuery {
@@ -19,11 +20,17 @@ export const Empty = () => <div>Empty</div>
 export const Failure = ({ error }: CellFailureProps) => (
   <div style={{ color: 'red' }}>Error: {error?.message}</div>
 )
+
 export const Success = ({ articles }) => {
   return articles.map((article) => (
-    <article key={article.id}>
-      <h2>{article.title}</h2>
+    <article>
+      <header>
+        <h2>
+          <Link to={routes.article({ id: article.id })}>{article.title}</Link>
+        </h2>
+      </header>
       <div>{article.body}</div>
+      <div>Posted at: {article.createdAt}</div>
     </article>
   ))
 }
